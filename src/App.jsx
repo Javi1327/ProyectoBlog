20a91839cec1a37a7671d0bcd02cbcf724a01823
AguusRomero <romeroagusakd@gmail.com>
2025-01-04 20:46:54 -0300

    Agrego componentes MisBlogs y Eliminar (Falta terminar)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import DetalleBlog from './pages/Home/DetalleBlog'
 import CrearBlog from './pages/CrearBlog/CrearBlog'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import MisBlogs from './pages/mis-blogs/MisBlogs'
+
 
 
 
@@ -26,7 +28,8 @@ function App() {
           <Route path='/' element={<Home/>} /> 
           <Route path='/blogs/:id' element={<DetalleBlog/>} />      
           <Route path='/crear-blog' element={<CrearBlog/>} />  
-          <Route path='/mis-blogs' element={<Prueba message="(falta hacer el Componente )" />} />   
+          <Route path='/modificar-blog/:idblog' element={<CrearBlog/>} />
+          <Route path='/mis-blogs/' element={<MisBlogs/>} />
           <Route path='/login' element={<Login/>} />  
           <Route path='/register' element={<Register/>} />
         </Routes>
